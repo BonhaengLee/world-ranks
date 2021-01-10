c36fdcab9ee2a2c709599d0e7a82426a9553dc2b
BonhaengLee <59217352+BonhaengLee@users.noreply.github.com>
2021-01-11 02:20:39 +0900

    210111 21:41까지 countries.map

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,21 +1,21 @@
 import Head from "next/head";
 import styles from "./Layout.module.css";
 
-const Layout = ({ children }) => {
+const Layout = ({ children, title = "world Ranks" }) => {
     return (
         <div className={styles.container}>
             <Head>
-                <title>Create Next App</title>
+                <title>{title}</title>
                 <link rel="icon" href="/favicon.ico" />
             </Head>
 
-            <header>
+            <header className={styles.header}>
                 <img src="send.svg" height={24} width={175} />
             </header>
 
             <main className={styles.main}>{children}</main>
 
-            <footer className={styles.footer}>footer</footer>
+            <footer className={styles.footer}>sjwdev @ devchallenges.io</footer>
         </div>
     );
 };
